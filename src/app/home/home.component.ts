7971e588a482a10f7dce629f8bd7ef5ceabb771c
Nathan Pabst <Nathan.Pabst@ey.com>
2019-05-03 17:43:46 -0500

    added unsubscribe to prevent data leaks and preserve memory.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 
 // tslint:disable-next-line: import-blacklist
 import 'rxjs/Rx';
 // tslint:disable-next-line: import-blacklist
 import { Observer } from 'rxjs/Observer';
+// tslint:disable-next-line: import-blacklist
+import { Subscription } from 'rxjs/Rx';
 
 
 @Component({
@@ -12,16 +14,21 @@ import { Observer } from 'rxjs/Observer';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  numbersObsSubcription: Subscription;
+  customObsSubcription: Subscription;
+
+
 
   constructor() { }
 
   ngOnInit() {
-    // const myNumbers = Observable.interval(1000);
-    // myNumbers.subscribe(
-    //   (number: number) => {
-    //     console.log(number);
-    //   })
+    const myNumbers = Observable.interval(1000);
+    this.numbersObsSubcription = myNumbers.subscribe(
+      (number: number) => {
+        console.log(number);
+      }
+    );
 
     const myObservable = Observable.create((observer: Observer<string>) => {
       setTimeout(() => {
@@ -39,11 +46,16 @@ export class HomeComponent implements OnInit {
         observer.next('third package');
       }, 6000);
     });
-    myObservable.subscribe(
+    this.customObsSubcription = myObservable.subscribe(
       (data: string) => { console.log(data); },
       (error: string) => { console.log(error); },
       () => { console.log('completed'); }
       );
   }
 
+  // cleans up space and memory and elimates data leaks
+  ngOnDestroy() {
+    this.numbersObsSubcription.unsubscribe();
+    this.customObsSubcription.unsubscribe();
+  }
 }
