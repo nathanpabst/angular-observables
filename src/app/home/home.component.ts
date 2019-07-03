676e8eb0b4bc0e10cfcda34ced73eb220115cdb4
Nathan Pabst <Nathan.Pabst@ey.com>
2019-07-03 12:47:44 -0500

    using rxjs operators to transform data.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription, interval, Observable } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -12,9 +13,6 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    // this.firstObsSubscription = interval(1000).subscribe(count => {
-    //   console.log(count);
-    // });
     const customIntervalObservable = Observable.create(observer => {
       let count = 0;
       setInterval(() => {
@@ -28,15 +26,21 @@ export class HomeComponent implements OnInit, OnDestroy {
         count++;
       }, 1000);
     });
-    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
-      console.log(data);
-    }, error => {
-      console.log(error);
-      alert(error.message);
-    }, () => {
-      console.log('completed!');
-      alert('observable completed');
-    });
+
+    this.firstObsSubscription = customIntervalObservable.pipe(
+      filter(data => {
+        return data > 0;
+      }), map((data: number) => {
+        return 'Round: ' + (data + 1);
+      })).subscribe(data => {
+        console.log(data);
+      }, error => {
+        console.log(error);
+        alert(error.message);
+      }, () => {
+        console.log('completed!');
+        alert('observable completed');
+      });
   }
 
   ngOnDestroy() {
